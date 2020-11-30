119b3d8892496c1c3619ac47fd0cb6bd14bb9a3f
icandell <47439788+icandell@users.noreply.github.com>
2020-11-29 23:50:28 -0500

    Update _navigation.php

--- _navigation.php
+++ _navigation.php
@@ -1,7 +1,6 @@
             <div class="space">
                 <a href="index.php">Public</a>
                 |
-                <a href="doctor.php">Doctor</a>
+                <a href="siteOwner.php">Site Owner</a>
                 |
-                <a href="manager.php">Manager</a>
             </div>
